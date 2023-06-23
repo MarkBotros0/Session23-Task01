44819ca69428313593ad3897573ac52510e444c8
markkhalil <mark3essam@gmail.com>
2023-06-24 01:36:11 +0300

    added redux-thunk and fetched data in moviesAction.js

--- react-redux-movies/src/components/View.js
+++ react-redux-movies/src/components/View.js
@@ -2,40 +2,15 @@ import React, { useEffect } from 'react'
 import Movies from './Movies'
 import Stats from './Stats'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchMoviesFailure, fetchMoviesRequest, fetchMoviesSuccess } from '../redux/movies/moviesActions'
+import fetchMovies from '../redux/movies/moviesActions'
 
 const View = () => {
 
     const page = useSelector(state => state.page.page)
     const dispatch = useDispatch()
 
-    const fetchMovies = () => {
-        dispatch(fetchMoviesRequest())
-        fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, {
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTQ2MzgxMDgzNzliODFjZWNiYTE4ZmI4MDMzZTBiNSIsInN1YiI6IjY0NzczZTc3MDA1MDhhMDExNmQ1NTViNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5aEDM2F7O2mNwqxa-ktSn9xPYzgqNlL-KLaNEyHQxfg'
-            }
-        })
-            .then((result) => result.json())
-            .then((response) => {
-                dispatch(fetchMoviesSuccess({ movies: response.results, topRatedMovie: getTopRated(response.results) }))
-            })
-            .catch((error) => {
-                dispatch(fetchMoviesFailure(error))
-            })
-    }
-
-    const getTopRated = (allMovies) => {
-        return allMovies.reduce(
-            (prev, current) => {
-                return prev.vote_average > current.vote_average ? prev : current
-            })
-    }
-
-
     useEffect(() => {
-        fetchMovies()
+        dispatch(fetchMovies(page))
     }, [page])
 
     return (
